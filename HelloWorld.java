18f9ecacc43afb5816626e0eb85718ae25213936
Santiago Hewett <santiago.hewett@stu.ocsb.ca>
2025-02-05 15:52:56 +0000

    HelloWorld Intro-03

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//This program greets my friend and asks them how they are
+//@author  Santiago Hewett
+//@version 1.0
+//@since   2025/02/05
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        //print greeting to friend
+        System.out.println("Hello, Remy Skelton.");
+        System.out.println("How are you?");
+    }
+}
